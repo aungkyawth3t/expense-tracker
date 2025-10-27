ea9af38353533ce8663feaab397c1f1214fc2eb5
aungkyawth3t <aungkyawthethimself@gmail.com>
2025-10-27 19:33:01 +0630

    login

--- src/Views/auth/login.php
+++ src/Views/auth/login.php
@@ -93,7 +93,7 @@
             <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
                 User Name
             </label>
-            <input class="bg-gray-50 py-2 px-3 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 block w-full <?= isset($errors['email']) ? 'border-red-500' : '' ?>" 
+            <input class="bg-gray-50 py-2 px-3 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 block w-full <?= isset($errors['username']) ? 'border-red-500' : '' ?>" 
                 id="email" 
                 type="text" 
                 placeholder="Enter your username"
